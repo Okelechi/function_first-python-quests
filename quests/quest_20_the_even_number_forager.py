d55d85df119fd2665449baeb3c1060a477c2b3eb
Noelle-1220 <m.ingabire3@alustudent.com>
2026-04-16 01:52:33 +0400

    quest 20 added with comments

--- quests/quest_20_the_even_number_forager.py
+++ quests/quest_20_the_even_number_forager.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#this quest was about using a for loop to find even numbers between 1 and 20
 for number in 1 to 20:
 	if (number mod 2) == 0:
 		print("The number is even: ", number}
