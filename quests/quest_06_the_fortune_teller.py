ffe92a44ca438561da500df5082859164007297a
Noelle-1220 <m.ingabire3@alustudent.com>
2026-04-16 01:46:59 +0400

    quest 6 done with added comments

--- quests/quest_06_the_fortune_teller.py
+++ quests/quest_06_the_fortune_teller.py
@@ -1,3 +1,4 @@
+#this quest was about getting user input and displaying it using f-string
 name=input("Enter your age: ")
 quest=input("Enter your quest: ")
 print(f"Good day. {name}, may you succeed in your quest; {quest}.")
