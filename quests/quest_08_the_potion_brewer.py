c873f2d6256acae42b7d32f5b0e6bd9a92bd61a6
Noelle-1220 <m.ingabire3@alustudent.com>
2026-04-16 01:49:02 +0400

    quest 8 done with added comments

--- quests/quest_08_the_potion_brewer.py
+++ quests/quest_08_the_potion_brewer.py
@@ -1,2 +1,3 @@
+#this quest was about doing some basic math operations on variables and displaying the result using f-string
 total = (3 * 10) + (5 * 3)
 print(f"The potion costs {total} gold coins. ")
