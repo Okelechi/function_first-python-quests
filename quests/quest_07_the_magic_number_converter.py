add4d043f7404f0e8ae6067fc6afd34ea6670d5c
Noelle-1220 <m.ingabire3@alustudent.com>
2026-04-16 01:47:57 +0400

    quest 7 done with comments added

--- quests/quest_07_the_magic_number_converter.py
+++ quests/quest_07_the_magic_number_converter.py
@@ -1,3 +1,4 @@
+#this quest was about getting user input and doing some basic math operations on it
 birth_year=input("Enter your year of birth: ")
 Age=2026-int(birth_year)
 print(f"your age is {Age}.")
