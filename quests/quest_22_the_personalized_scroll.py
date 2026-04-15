728f955ec863e437cb6102924d3c97f6d560f7b7
root <b.tinkouree@alustudent.com>
2026-04-15 13:20:52 -0700

    Funtions with parameters

---
+++ quests/quest_22_the_personalized_scroll.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def personalized_greeting(name, quest):
+
+user_name = input("Enter your name: ")
+user_quest = input("Enter your quest: ")
+
+personalized_greeting(user_name, user_quest)
+
