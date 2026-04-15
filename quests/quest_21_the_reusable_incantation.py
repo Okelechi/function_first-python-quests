bee788499816cdfb68dec6dfb23d295ef0807512
debbierussell <d.russellab@alustudent.com>
2026-04-16 00:43:26 +0400

    Added the 21st-quest to the repo

---
+++ quests/quest_21_the_reusable_incantation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Define the function
+def greet_adventurer():
+    print("Welcome, brave adventurer!")
+    print("Your quest awaits you!")
+    print("----------------------------")
+
+# Call it three times
+greet_adventurer()
+greet_adventurer()
+greet_adventurer()
+
+
