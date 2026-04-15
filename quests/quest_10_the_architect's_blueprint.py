30c96cbf6a1d9b1ef0ae5536aea23d8a752a1321
Noelle-1220 <m.ingabire3@alustudent.com>
2026-04-16 01:50:05 +0400

    quest10 done with added comments

--- quests/quest_10_the_architect's_blueprint.py
+++ quests/quest_10_the_architect's_blueprint.py
@@ -1,3 +1,4 @@
+#this quest was about getting user input and doing some basic math operations on it
 length =float(input("Enter length : "))
 width =float(input("Enter width : "))
 area = length * width 
